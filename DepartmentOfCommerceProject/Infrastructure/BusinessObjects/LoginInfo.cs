df1c47dc37f9a0f6a3e2a3a2b6b335d8aec59b45
vano hacker <v.gritskevich25@gmail.com>
2019-10-12 20:52:52 +0300

    added LoginCommand and LoginInfo classes

---
+++ DepartmentOfCommerceProject/Infrastructure/BusinessObjects/LoginInfo.cs
@@ -0,0 +1,38 @@
+﻿namespace DepartmentOfCommerceProject.Infrastructure.BusinessObjects
+{
+    class LoginInfo
+    {
+        private string login = "";
+        private string pass = "";
+        private string hash = "";
+
+        public bool IsLoginAdjusted { get; private set; } = false;
+        public bool IsPassAdjusted { get; private set; } = false;
+
+        public string Login
+        {
+            set
+            {
+                IsLoginAdjusted = true;
+                hash = Crypto.GetMd5Hash(value + ":" + pass);
+            }
+        }
+
+        public string Pass
+        {
+            set
+            {
+                IsPassAdjusted = true;
+                hash = Crypto.GetMd5Hash(login + ":" + value);
+            }
+        }
+
+        public string Hash
+        {
+            get
+            {
+                return hash;
+            }
+        }
+    }
+}
---
+++ DepartmentOfCommerceProject/Infrastructure/Commands/LoginCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using DepartmentOfCommerceProject.Infrastructure.BusinessObjects;
+
+namespace DepartmentOfCommerceProject.Infrastructure
+{
+    class LoginCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (!(parameter is LoginInfo))
+            {
+                throw new ArgumentException("parameter must be LoginInfo", "parameter");
+            }
+
+            LoginInfo info = parameter as LoginInfo;
+
+            return info.IsLoginAdjusted && info.IsPassAdjusted;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (!(parameter is LoginInfo))
+            {
+                throw new ArgumentException("parameter must be LoginInfo", "parameter");
+            }
+
+
+        }
+    }
+}
