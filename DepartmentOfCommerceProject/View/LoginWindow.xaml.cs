6890117c48cf04e338b31dcc6ae3e70554f9c2e2
vano hacker <v.gritskevich25@gmail.com>
2019-10-12 23:37:42 +0300

    zuzuzu

--- DepartmentOfCommerceProject/View/LoginWindow.xaml.cs
+++ DepartmentOfCommerceProject/View/LoginWindow.xaml.cs
@@ -10,14 +10,12 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using DepartmentOfCommerceProject.Infrastructure;
 
 namespace DepartmentOfCommerceProject.View
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для LoginWindow.xaml
     /// </summary>
     public partial class LoginWindow : Window
     {
@@ -25,17 +23,5 @@ namespace DepartmentOfCommerceProject.View
         {
             InitializeComponent();
         }
-
-        //private void LoginButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    string login = loginTextBox.Text;
-        //    string pass = passwordTextBox.Password;
-
-        //    loginTextBox.Text = "";
-        //    passwordTextBox.Password = "";
-
-        //    string hash = Crypto.GetMd5Hash(login + ":" + pass);
-        //    MessageBox.Show(hash);
-        //}
     }
 }
